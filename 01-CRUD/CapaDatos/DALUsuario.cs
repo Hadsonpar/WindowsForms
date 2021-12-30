8e82a0c9d381852b59e61652c48fef8a69621e0a
Hadsonpar <hadson1@gmail.com>
2021-12-29 20:51:45 -0500

    CRUD - Windows Forms con SQL Server

---
+++ 01-CRUD/CapaDatos/DALUsuario.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaDatos
+{
+    /// <summary>
+    /// Clase que conforman los métodos para el acceso a datos a través 
+    /// de los procedimientos almacenados "Store Procedure".
+    /// </summary>
+    public class DALUsuario
+    {
+        private DALConexion conexion = new DALConexion();
+
+        SqlDataReader dataReader;
+        DataTable table = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        /// <summary>
+        /// Método para Insertar "Create" los datos
+        /// </summary>
+        public void CreateUsuario(string usuario, string contrasena, int intentos, double nivelSeg, DateTime fechaReg)
+        {
+            comando.Connection = conexion.OpenConnection();
+            comando.CommandText = "SP_INSERTAR_USUARIO";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@usuario", usuario);
+            comando.Parameters.AddWithValue("@contrasena", contrasena);
+            comando.Parameters.AddWithValue("@intentos", intentos);
+            comando.Parameters.AddWithValue("@nivelSeg", nivelSeg);
+            comando.Parameters.AddWithValue("@fechaReg", fechaReg);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+        }
+
+        /// <summary>
+        /// Método para Seleccionar "Read" los datos
+        /// </summary>
+        public DataTable ReadAllUsuario() 
+        {        
+            comando.Connection = conexion.OpenConnection();
+            comando.CommandText = "SP_SELECCIONAR_ALL_USUARIO";
+            comando.CommandType = CommandType.StoredProcedure;
+            dataReader = comando.ExecuteReader();
+            table.Load(dataReader);
+            conexion.CloseConnection();
+            return table;            
+        }
+
+        /// <summary>
+        /// Método para Actualizar "Update" los datos
+        /// </summary>
+        /// <param name="id">parámetro para filtra y actualizar el registro de acurdo al id</param>
+        public void UpdateUsuario(string usuario, string contrasena, int intentos, double nivelSeg, DateTime fechaReg, int id)
+        {
+            
+            comando.Connection = conexion.OpenConnection();
+            comando.CommandText = "SP_ACTUALIZAR_USUARIO";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@usuario", usuario);
+            comando.Parameters.AddWithValue("@contrasena", contrasena);
+            comando.Parameters.AddWithValue("@intentos", intentos);
+            comando.Parameters.AddWithValue("@nivelSeg", nivelSeg);
+            comando.Parameters.AddWithValue("@fechaReg", fechaReg);
+            comando.Parameters.AddWithValue("@id",id);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+        }
+
+        /// <summary>
+        /// Método para Eliminar "Delete" los datos
+        /// </summary>
+        /// <param name="id">parámetro de entrada para eliminar el registro de acuerdo al filtro id</param>
+        public void DeleteUsuario(int id) {
+            comando.Connection = conexion.OpenConnection();
+            comando.CommandText = "SP_ELIMINAR_USUARIO";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+        }
+
+    }
+}
---
+++ 01-CRUD/CapaNegocio/BLLUsuario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using CapaDatos;
+
+namespace CapaNegocio
+{
+    public class BLLUsuario
+    {
+        private DALUsuario objetoCD = new DALUsuario();
+
+        public DataTable View() {
+
+            DataTable tabla = new DataTable();
+            tabla = objetoCD.ReadAllUsuario();
+            return tabla;
+        }
+        public void Create(string usuario, string contrasena, int intentos, double nivelSeg, DateTime fechaReg)
+        {
+
+            objetoCD.CreateUsuario(usuario, contrasena, Convert.ToInt32(intentos),Convert.ToDouble(nivelSeg), fechaReg);
+    }
+
+        public void Update(string usuario, string contrasena, int intentos, double nivelSeg, DateTime fechaReg, int id)
+        {
+            objetoCD.UpdateUsuario(usuario, contrasena, Convert.ToInt32(intentos), Convert.ToDouble(nivelSeg), 
+                fechaReg, Convert.ToInt32(id));
+        }
+
+        public void Delete(int id) {
+
+            objetoCD.DeleteUsuario(id);
+        }
+
+    }
+}
